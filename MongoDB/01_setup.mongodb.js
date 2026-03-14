2b97291bbe480651b7935edd38e584729ee2a35a
Kartik Dhomne <kartikdhomne1997@gmail.com>
2026-03-14 23:59:59 +0530

    update

---
+++ MongoDB/01_setup.mongodb.js
@@ -0,0 +1,54 @@
+https://cwh-full-next-space.fra1.cdn.digitaloceanspaces.com/YouTube/MongoDB%20Handbook.pdf
+
+use("ecommerce");
+
+db.products.insertMany([
+  {
+    name: "Wireless Mouse",
+    price: 799,
+    category: "Electronics",
+    stock: 120,
+    ratings: 4.5,
+    tags: ["computer", "accessory", "wireless"],
+    createdAt: new Date(),
+  },
+  {
+    name: "Mechanical Keyboard",
+    price: 2499,
+    category: "Electronics",
+    stock: 80,
+    ratings: 4.8,
+    tags: ["keyboard", "mechanical"],
+    createdAt: new Date(),
+  },
+  {
+    name: "Gaming Laptop",
+    price: 85999,
+    category: "Computers",
+    stock: 30,
+    ratings: 4.6,
+    tags: ["gaming", "laptop"],
+    createdAt: new Date(),
+  },
+]);
+
+db.contacts.insertMany([
+  {
+    name: "Alice",
+    message: "Loved your website!",
+    phone: "9876543210",
+    createdAt: new Date(),
+  },
+  {
+    name: "Bob",
+    message: "Do you have discounts on laptops?",
+    phone: "9123456789",
+    createdAt: new Date(),
+  },
+  {
+    name: "Carol",
+    message: "I want to cancel my order.",
+    phone: "9988776655",
+    createdAt: new Date(),
+  },
+]);
