9f5552850996d562bcccb04cf76dcc24f5f53db4
Kartik Dhomne <kartikdhomne1997@gmail.com>
2026-04-25 23:52:00 +0530

    update

--- DSA-2026/Basic/reverse-string-without-method.js
+++ DSA-2026/Basic/reverse-string-without-method.js
@@ -25,3 +25,34 @@ function ReverseStringRecursion(str) {
 }
 
 console.log(ReverseStringRecursion(str))
+
+let str = "Hello, World!";
+
+function reverseString(str) {
+  let reversed = "";
+
+  for (let i = str.length - 1; i >= 0; i--) {
+    reversed = reversed + str[i];
+  }
+
+  return reversed;
+}
+
+// OR
+
+function reverseStringOne(str) {
+  let left = 0;
+  let right = str.length - 1;
+
+  while (left < right) {
+    let temp = str[left];
+    str[left] = str[right];
+    str[right] = temp;
+
+    left++;
+    right--;
+  }
+  return str;
+};
+console.log(reverseString(str));
+console.log(reverseStringOne(str));
