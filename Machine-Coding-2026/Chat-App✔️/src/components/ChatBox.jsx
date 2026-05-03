a6a6a51391d09b6dd96ac5e38725f48ae6a2d95c
Kartik Dhomne <kartikdhomne1997@gmail.com>
2026-05-03 17:05:30 +0530

    Update

--- Machine-Coding-2026/Chat-App✔️/src/components/ChatBox.jsx
+++ Machine-Coding-2026/Chat-App✔️/src/components/ChatBox.jsx
@@ -6,9 +6,7 @@ const ChatBox = ({ messages, onSend, messageBoxRef }) => {
   return (
     <div className="chat-container">
       <h2>Chat App</h2>
-
       <MessageList messages={messages} messageBoxRef={messageBoxRef} />
-
       <MessageInput onSend={onSend} />
     </div>
   );
